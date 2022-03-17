4cd6969c6b963b58bd16eacbc8421082816eb9e0
DmitryStetsenko <ebusines2010@gmail.com>
2022-03-17 12:11:00 +0200

    create PostForm component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+
 
 import './styles/App.css';
 
@@ -13,32 +12,13 @@ const App = () => {
         {id: 3, title: 'JavaScript3', body: 'description'}
     ]);
 
-	const [post, setPost] = useState({title: '', body: ''});
-
-	const addNewPost = (e) => {
-		e.preventDefault();
-
-		setPosts([...posts, {...post, id: Date.now()}]);
-		setPost({title: '', body: ''});
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost]);
 	}
 
 	return (
 		<div className="App">
-			<form>
-				<MyInput 
-					value={ post.title }
-					onChange={ e => setPost({...post, title: e.target.value}) }
-					type="text" 
-					placeholder="Название поста"
-				/>
-				<MyInput
-					value={ post.body }
-					onChange={ e => setPost({...post, body: e.target.value}) }
-					type="text" 
-					placeholder="Описание поста" 
-				/>
-				<MyButton onClick={ addNewPost }>Создать пост</MyButton>
-			</form>
+			<PostForm create={ createPost }/>
 			<PostList posts={ posts } title="Список постов" />
 		</div>
 	);
